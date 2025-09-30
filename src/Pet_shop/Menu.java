e1c6febb1d464a5bcec0a33aa07588fd1fa7d58d
“crissmcoelho” <crissmcoelho@yahoo.com.br>
2025-09-30 11:07:39 -0300

    :sparkles: Insercao Classe Menu e Cores

---
+++ src/Pet_shop/Menu.java
@@ -0,0 +1,103 @@
+package Pet_shop;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import Pet_shop.util.Cores;
+
+public class Menu {
+
+	private static final Scanner leia = new Scanner(System.in);
+	int opcao;
+
+	
+	public static void main(String[] args) {
+
+		int opcao;
+
+		
+
+		while (true) {
+
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+					+ "*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            PET SHO CRISTINA                         ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Produto                        ");
+			System.out.println("            2 - Listar todos os Produtos             ");
+			System.out.println("            3 - Buscar Produto por ID                ");
+			System.out.println("            4 - Atualizar Dados do Produto           ");
+			System.out.println("            5 - Apagar Produto                       ");
+			System.out.println("            0 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     " + Cores.TEXT_RESET);
+
+			try {
+				opcao = leia.nextInt();
+				leia.nextLine();
+			} catch (InputMismatchException e) {
+				opcao = -1;
+				System.out.println("\nDigite um número inteiro entre 0 e 8");
+				leia.nextLine();
+			}
+
+			if (opcao == 0) {
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nPet Shop Cristina - Cuidamos com amor do seu Pet!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+			case 1:
+				System.out.println(Cores.TEXT_WHITE + "Criar Produto\n\n");
+
+				keyPress();
+				break;
+			case 2:
+				System.out.println(Cores.TEXT_WHITE + "Listar todos os Produtos\n\n");
+
+				keyPress();
+				break;
+			case 3:
+				System.out.println(Cores.TEXT_WHITE + "Consultar dados do Produto - por ID\n\n");
+
+				keyPress();
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_WHITE + "Atualizar dados do Produto\n\n");
+
+				keyPress();
+				break;
+			case 5:
+				System.out.println(Cores.TEXT_WHITE + "Apagar o Produto\n\n");
+
+				keyPress();
+				break;
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+				keyPress();
+				break;
+			}
+		}
+
+	}
+
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: Cristina Martins Coelho ");
+		System.out.println("github.com/crissmcoelho");
+		System.out.println("*********************************************************");
+	}
+
+	public static void keyPress() {
+		System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para continuar...");
+		leia.nextLine();
+	}
+
+}
